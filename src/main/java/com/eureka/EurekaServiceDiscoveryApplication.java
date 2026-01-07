b4b31285b5f97df261a5f1ed82e8ebee07307219
silvi <silviagfernandes1@gmail.com>
2026-01-07 14:40:18 -0300

    feat: config service discovery eureka

--- src/main/java/com/eureka/EurekaServiceDiscoveryApplication.java
+++ src/main/java/com/eureka/EurekaServiceDiscoveryApplication.java
@@ -1,9 +1,12 @@
 package com.eureka;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServiceDiscoveryApplication {
 
 	public static void main(String[] args) {
