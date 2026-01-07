6cd196f9111015bdb9f31be38755ca985176d222
silvi <silviagfernandes1@gmail.com>
2026-01-07 12:56:31 -0300

    feat: first commit

---
+++ src/main/java/com/eureka/EurekaServiceDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceDiscoveryApplication.class, args);
+	}
+
+}
